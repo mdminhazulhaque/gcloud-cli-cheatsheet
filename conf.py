b675684073ef312aa2b22dfaeb84fe51a34296e1
Md. Minhazul Haque <mdminhazulhaque@gmail.com>
2024-10-11 22:01:08 +0600

    Add favicon

--- conf.py
+++ conf.py
@@ -6,6 +6,7 @@ extensions = [
   'sphinx_rtd_theme',
   'sphinxemoji.sphinxemoji',
   'sphinxext.opengraph',
+  'sphinx_favicon',
 ]
 
 templates_path = ['_templates']
@@ -14,5 +15,9 @@ exclude_patterns = ['build.sh', '.github/*', 'env/*', 'requirements.txt']
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 html_extra_path = ['CNAME', '.nojekyll']
+
+favicons = [
+    {"href": "favicon.ico"},
+]
 
 ogp_image = "/_static/mitchell-luo-XQjovx-X5-k-unsplash.jpg"
